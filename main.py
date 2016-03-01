3dc4da65707f7125592232e01ad4cd9a5df76384
Abhilash <Abhilash>
2016-02-29 20:07:40 -0500

    changes

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import requests
 
 url = 'http://www.ionomicshub.org/arabidopsis/ajax/DataSearchJson.action'
 #www.ionomicshub.org/arabidopsis/ajax/DataSearchJson.action?ATGNum=AT2G25360&Skip=3&Limit=1
-
-def search(atgnum, skip, limit):
-	payload  = {'ATGNum': atgnum, 'Skip': skip, 'Limit': limit}
+#{'atgnum':'AT2G25360', 'skip':'3', 'limit':'1'}
+def search(args):
+	payload  = {'ATGNum': args['atgnum'], 'Skip': args['skip'], 'Limit': args['limit']}
 	response = requests.get(url, params=payload)
 	#print response.json()
 	print response.text
